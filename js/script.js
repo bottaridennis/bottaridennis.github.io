70cf515e74d3b3a87048145c9feb611e9b007feb
DedeNeko <dennisbottari@gmail.com>
2024-03-27 13:51:38 +0100

    Push

--- js/script.js
+++ js/script.js
@@ -34,7 +34,8 @@ window.onscroll = () => {
     header.classList.toggle('sticky', window.scrollY > 100);
 
     // remove toggle icon and navbar when click navbar links (scroll)
-
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 
     // animation footer on scroll
 

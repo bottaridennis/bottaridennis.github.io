7be99c1574da57245a2a3158f126df7d2f483b0d
Dennis Bottari <dennisbottari@gmail.com>
2026-04-22 00:40:34 +0200

    authorize

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: './',
+    base: '/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
